8f0b7f5f5bc1eb5a1e794497d14560d032bf8dca
Matthieu Dartiailh <marul@laposte.net>
2015-02-03 16:53:05 +0100

    Add back Luke test of a function followed by its concrete implementation.

--- tests/headers/functions.h
+++ tests/headers/functions.h
@@ -2,10 +2,18 @@
 
 //Defining a standard function.
 void f(int, int);
-inline int g(char *ch, char **str);
+
+// Defining a function with its implementation following
+inline int g(char *ch, char **str)
+{
+     JUNK
+     { }
+     int localVariable = 1;
+}
 
 // Defining a function pointer.
 int(*fnPtr)(char, float);
+
 
 // Adding dllexport and stdcall annotation to a function.
 int __declspec(dllexport) __stdcall function1();
